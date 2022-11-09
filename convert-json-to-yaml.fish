2b894dd467d6f76a43867498ade141bca6f92392
Daniel D. Beck <daniel@ddbeck.com>
2022-11-09 19:02:48 +0100

    Improve handling of YAML after the fact

--- convert-json-to-yaml.fish
+++ convert-json-to-yaml.fish
@@ -1,5 +1,16 @@
 #!/usr/bin/env fish
 
+begin
+    command -q yq
+    and command -q sponge
+    and command -q rg
+    and command -q splarf
+end
+or begin
+    echo -e "Sorry, this script is not very portable. You need:\n - yq\n - rg (ripgrep)\n - sponge\nto run this script."
+    exit 1
+end
+
 set -l json_source_dirs query-results web-platform-features
 
 for dir in $json_source_dirs
